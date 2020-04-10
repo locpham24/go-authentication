45f463173295fb9c00baf77a2a804a2a1f33623b
TinDT <tin.duong@production.com.vn>
2020-04-10 18:09:44 +0700

    feat: authenticate a user

--- handler/auth.go
+++ handler/auth.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"github.com/locpham24/go-authentication/model"
+	"net/http"
+	"os"
+	"time"
 )
 
 type AuthHandler struct {
@@ -74,5 +78,56 @@ func (a AuthHandler) register(c *gin.Context) {
 }
 
 func (a AuthHandler) login(c *gin.Context) {
-	c.JSON(200, nil)
+	// 1. get user info from request
+	input := model.UserForm{}
+	if err := c.BindJSON(&input); err != nil {
+		c.JSON(400, gin.H{
+			"code":    1000,
+			"message": err.Error(),
+		})
+		return
+	}
+
+	user := model.User{}
+	user.Fill(input)
+
+	// 2. verify if user exist
+	err := a.DB.First(&user, "username = ?", user.Username).Error
+	if err != nil {
+		c.JSON(404, gin.H{
+			"code":    2000,
+			"message": err.Error(),
+		})
+		return
+	}
+
+	// 3. check if password correct
+	if input.Password != user.Password {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"code":    2000,
+			"message": "Invalid password",
+		})
+		return
+	}
+
+	// 4. create JWT token with expired time in 24h
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &model.Claims{
+		Username: input.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":    1000,
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, tokenString)
 }
